1a4927345f500dc0ca39ab1d50afe41738715575
jonathannqy <jonathaanxsouza10@gmail.com>
2026-06-26 23:02:49 -0300

    Bloom.h criada e alimentada

---
+++ src/bloom.h
@@ -0,0 +1,19 @@
+#ifndef BLOOM_H
+#define BLOOM_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct {
+    int m; //Tamanho de bits
+    int k; //Funcões hash
+    char *vetor; //Aponta para a memória
+}Filtrodebloom;
+
+//funções filtro de bloom de (criar, liberar(free), inserir e de consultar o filtro)
+Filtrodebloom criar_filtro(int n);
+void liberar_filtro(Filtrodebloom *bloom);
+void inserir_bloom(Filtrodebloom *bloom, char *item);
+int consultar_bloom(Filtrodebloom *bloom, char *item);
+
+#endif
