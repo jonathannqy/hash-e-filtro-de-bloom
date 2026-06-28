1bb81cbd12060a21b35b4a86ac683de44e461f84
Otavio Linhares <testeotavio124@gmail.com>
2026-06-28 15:51:19 -0300

    (code) Organizando

--- src/auxiliares.c
+++ src/auxiliares.c
@@ -4,8 +4,8 @@
 void menu_principal() {
     printf("\n--- Sistema de Verificação de Cadastro ---\n");
     printf("[1] Inserir novo usuário no hash e no bloom\n");
-    printf("[2] Consultar usuário (com bloom)\n"); // <- Nome atualizado para clareza
-    printf("[3] Consultar usuário (sem bloom)\n");   // <- NOVA OPÇÃO AQUI
+    printf("[2] Consultar usuário (com bloom)\n"); 
+    printf("[3] Consultar usuário (sem bloom)\n");
     printf("[4] Exibir estatísticas\n");
     printf("[5] Inserir lote na tabela hash e no bloom\n");
     printf("[6] Gerar lote (Arquivo)\n");
